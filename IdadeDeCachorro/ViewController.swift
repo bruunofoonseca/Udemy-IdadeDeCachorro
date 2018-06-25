7a85469b3c01a2ed0a7811dcc3bbe2354eaf6bed
Bruno Fonseca de Almeida <bruno.almeida@m-rend-sist-01.local>
2018-06-25 14:20:59 -0300

    Initial Commit

---
+++ IdadeDeCachorro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdadeDeCachorro
+//
+//  Created by Bruno Fonseca de Almeida on 25/06/2018.
+//  Copyright © 2018 Bruno Fonseca de Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
